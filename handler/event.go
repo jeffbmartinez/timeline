9de08c95dfa98d41543016950929ad52faec789a
jeff martinez <jeffbmartinez@gmail.com>
2015-07-09 01:15:07 -0600

    delete commented out code

--- handler/event.go
+++ handler/event.go
@@ -28,44 +28,6 @@ func getEvent(response http.ResponseWriter, request *http.Request) {
 	notImplemented(response, request)
 }
 
-// func postEvent(response http.ResponseWriter, request *http.Request) {
-// 	urlArgs, err := request.URL.Query()
-// 	log.Infof("Event storage request with args: %v", urlArgs)
-
-// 	REQUIRED_ARGS := []string{
-// 		"series",
-// 	}
-
-// 	missingArgs := GetMissingArgs(urlArgs, REQUIRED_ARGS)
-
-// 	if len(missingArgs) > 0 {
-// 		errorMessage := fmt.Sprintf("Missing required arguments: %v", missingArgs)
-// 		log.Infof(errorMessage)
-// 		WriteSimpleResponse(response, errorMessage, http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	point := newSimplePoint(urlArgs)
-
-// 	influxDbConfig, err := influxdb.GetConfiguration()
-// 	if err != nil {
-// 		log.Errorf("Problem reading influxdb configuration: %v", err)
-// 		WriteSimpleResponse(response, "Unable to store event", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	const RETENTION_POLICY string = "default"
-
-// 	err = influxdb.StorePoint(point, influxDbConfig.DbName, RETENTION_POLICY)
-// 	if err != nil {
-// 		log.Errorf("Problem writing to influxdb: %v", err)
-// 		WriteSimpleResponse(response, "Unable to store event", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	WriteSimpleResponse(response, "event recorded", http.StatusOK)
-// }
-
 func postEvent(response http.ResponseWriter, request *http.Request) {
 	urlArgs, err := parseUrlArgs(request.URL.Query())
 	if err != nil {
